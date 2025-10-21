808414fdac3091a0ef8b15710988a399adc66441
kprincipe <kinanprincipes@gmail.com>
2025-10-21 14:18:38 -0400

    Implementacao exercicio 8 da lista de Strings

---
+++ 08_apagar_ocorrencia.c
@@ -0,0 +1,21 @@
+// Crie uma função que recebe uma string e um caractere,
+// e apague todas as ocorrências desses caractere na string.
+
+#include <stdio.h>
+
+#define TAM 256
+
+void ler_string(char *str);
+void eliminar_caractere(char *str, char c);
+
+int main(void) {
+    char str[TAM];
+}
+
+void ler_string(char *str) {
+    printf("NAO IMPLEMENTADO\n");
+}
+
+void eliminar_caractere(char *str, char c) {
+    printf("NAO IMPLEMENTADO\n");
+}
